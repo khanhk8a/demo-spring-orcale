eb1578859d7a209d3bcf0151cd53af3ddc9b5778
khanhk8a <khanhk8a@gmail.com>
2021-05-16 20:04:07 +0700

    LoginRestApi

--- src/main/java/com/example/demospringoracle/api/LoginRestApi.java
+++ src/main/java/com/example/demospringoracle/api/LoginRestApi.java
@@ -15,7 +15,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
-public class LodaRestApi {
+public class LoginRestApi {
 
     @Autowired
     AuthenticationManager authenticationManager;
